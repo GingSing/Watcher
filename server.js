d78c35aa1d8e99e1038751c5aa92198f5afa334a
Gary Yue <garyyue2017@gmail.com>
2019-02-25 14:51:20 -0800

    first

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ app.use('/movies', movies);
 
 const PORT = process.env.PORT || 5000;
 
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    
+    //Set static folder
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 app.listen(PORT,() => {
     console.log(`Listening on port ${PORT}`);
 });
