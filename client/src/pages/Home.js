bf514514dc53352b5dfb561c452edc3e7579a515
Gary Yue <garyyue2017@gmail.com>
2019-03-01 01:50:39 -0800

    removed for proxy

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 class Home extends Component{
     componentDidMount(){
-        fetch('http://localhost:5000/api/movies/trending')
+        fetch('/api/movies/trending')
             .then(res => res.json())
             .then(data => console.log(data))
             .catch(err => console.log(err));
